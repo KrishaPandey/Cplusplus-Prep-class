e1c50b6bca07acb37a19752acbc4443e16c6a17b
krisha009 <krisha.pandey@lunivatech.com>
2025-05-04 08:54:14 +0545

    Question added

---
+++ oop/polymorphism/question.cpp
@@ -0,0 +1,41 @@
+
+/*
+ 1. Define a class Shape.
+
+//In the Shape class, declare a virtual function draw() that 
+will print the message "Drawing a shape" by default.
+
+//Declare another virtual function area() 
+that will return 0.0.
+*/
+
+/*
+2. Create a class Circle that inherits from Shape.
+
+In the Circle class:
+
+Add a private member radius to store the radius of the circle.
+
+Create a constructor to initialize the radius.
+
+Override the draw() function to print "Drawing a Circle".
+
+Override the area() function to calculate the area of the circle  
+*/
+
+/*
+3. Create a class Rectangle that inherits from Shape.
+
+In the Rectangle class:
+
+Add two private members, width and height, to represent 
+the dimensions of the rectangle.
+
+Create a constructor to initialize width and height.
+
+Override the draw() function to print "Drawing a Rectangle".
+
+Override the area() function to calculate the area of the rectangle.
+*/ 
+
+
